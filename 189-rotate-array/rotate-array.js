d0ca680668236c8b456d6e337232d0dc7201fe97
TOUSIF <ajtousif71@gmail.com>
2026-09-24 19:00:11 +0600

    Time: 10 ms (38.96%) | Memory: 96 MB (39.82%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    
+};
+
+var rotate = function(nums,k){
+    k = k % nums.length;
+
+    let last = nums.splice(nums.length -k);
+    nums.unshift(...last);
+
+}
